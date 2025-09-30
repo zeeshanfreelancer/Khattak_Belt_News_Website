ab08ba00c1f09929680416048108d9a2c20ebf58
Zeeshan <zeeshankhan469224@gmail.com>
2025-09-30 23:45:40 +0500

    Update port

--- server/server.js
+++ server/server.js
@@ -84,7 +84,7 @@ app.use((err, req, res, next) => {
 });
 
 // Server Init
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 connectDB().then(() => {
   const server = app.listen(PORT, () => {
